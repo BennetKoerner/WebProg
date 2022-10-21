1a25377feea44d168c25c61d2742bedf4bdc6257
Bennet-Koerner <bennet.koerner@de.markant.com>
2022-10-21 15:43:41 +0200

    Changes by Heuberger Lukas

--- Website/website.js
+++ Website/website.js
@@ -1,28 +1,30 @@
 
-
-let addCarItemButtons = document.getElementsByClassName("btn-primary")
-console.log(addCarItemButtons)
-
-
-for (let i = 0; i < addCarItemButtons.length; i++) {
-    let button = addCarItemButtons[i]
-    button.addEventListener("click", function(event) {
-        let buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.add()
-        updateCartTotal()
-    })
-}
-
-function updateCartTotal() {
-    let cardRowContainer = document.getElementsByClassName("row")[0]
-    let cards = cartRowContainer.getElementsByClassName("card")
-    let total = 0;
-    for (let i = 0; i < cards.length; i++) {
-        let card = cards[i]
-        let priceElement = card.getElementsByClassName("card-tex")[0]
-        let price = parseFloat(priceElement.innerText.replace("€", ""))
-        price = price.innerText.replace("Preis: ", " ")
+    let addCardItemButtons = document.getElementsByClassName("btn btn-primary")
+    console.log(addCardItemButtons)
+    for (let i = 0; i < addCardItemButtons.length; i++) {
+        let button = addCardItemButtons[i]
+        button.addEventListener("click", addCardItem)
     }
-    document.getElementsByClassName("fw-semibold")[0].innerText = total + "€"
-}
+
+
+    function addCardItem(event) {
+        let buttonClicked = event.target
+        buttonClicked.get.add()
+        updateCartTotal()
+    }
+
+    function updateCartTotal() {
+        let cardRowContainer = document.getElementsByClassName("row")[0]
+        let cards = cardRowContainer.getElementsByClassName("card")
+        let total = 0;
+        for (let i = 0; i < cards.length; i++) {
+            let card = cards[i]
+            let priceElement = card.getElementsByClassName("card-tex")[0]
+            let price = parseFloat(priceElement.innerText.replace("€", ""))
+            price = price.innerText.replace("Preis: ", " ")
+        }
+        document.getElementsByClassName("fw-semibold")[0].innerText = total + "€"
+    }
+
+
 
